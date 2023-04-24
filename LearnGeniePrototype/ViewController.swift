ab778f69ef7da294cfbba47d4732f6334d9e4e38
artyaz <achmilenko58@gmail.com>
2023-04-24 22:41:09 +0300

    Initial Commit

---
+++ LearnGeniePrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnGeniePrototype
+//
+//  Created by Артем Чмиленко on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
